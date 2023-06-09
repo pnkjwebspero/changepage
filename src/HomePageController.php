1de90e7924686ab3424570f557cc67e87a1fb6f0
karan <karan@webspero.com>
2023-06-09 17:02:20 +0530

    create new file inside resource folder

--- src/HomePageController.php
+++ src/HomePageController.php
@@ -5,6 +5,8 @@ namespace Pnkjwebspero\Changepage;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
 
 class HomePageController extends Controller
 {
@@ -17,7 +19,7 @@ class HomePageController extends Controller
     {
         $fileName = "testProvider.php";
         $content = "This Provider for testing!";
-        $filePath = resource_path('path/to/resources/folder/') . $fileName;
+        $filePath = resource_path($fileName);
         File::put($filePath, $content);
     }
 }

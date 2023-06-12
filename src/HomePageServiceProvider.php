208f3856b66191270c5a6e1bf918891dec4c512d
karan <karan@webspero.com>
2023-06-12 11:43:03 +0530

    changes in provider

--- src/HomePageServiceProvider.php
+++ src/HomePageServiceProvider.php
@@ -4,20 +4,32 @@ namespace Pnkjwebspero\Changepage;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
 
 class HomePageServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-       include __DIR__.'/routes.php';
+    //    include __DIR__.'/routes.php';
     }
 
+    // public function register()
+    // {
+    //     // $this->app->singleton('file-adder', function () {
+    //     //     return new \Changepage\Src\Changepage();
+    //     // });
+    //     $this->app->make('Pnkjwebspero\Changepage\HomePageController');
+    //     $this->loadViewsFrom(__DIR__.'/views','changepage');
+    // }
+
     public function register()
-    {
-        // $this->app->singleton('file-adder', function () {
-        //     return new \Changepage\Src\Changepage();
-        // });
-        $this->app->make('Pnkjwebspero\Changepage\HomePageController');
-        $this->loadViewsFrom(__DIR__.'/views','changepage');
+{
+    if ($this->app->runningInConsole()) {
+        $resourcePath = resource_path('new-folder');
+        if (!File::exists($resourcePath)) {
+            File::makeDirectory($resourcePath);
+        }
+        File::put($resourcePath . '/new-file.txt', 'This is a new file created by composer update.');
     }
+}
 }

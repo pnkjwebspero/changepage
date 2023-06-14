bdaa8e8783013ff16ab116fdf0d7af57c30f618a
karan <karan@webspero.com>
2023-06-14 15:39:18 +0530

    commit 13

--- src/ButtonCommand.php
+++ src/ButtonCommand.php
@@ -38,6 +38,10 @@ class ButtonCommand extends Command
         'auth.context.js' => 'auth.context.jsx',
     ];
 
+    protected $rootJSFiles = [
+        'app.js' => 'app.jsx',
+    ];
+
     /**
      * Execute the console command.
      *
@@ -67,10 +71,6 @@ class ButtonCommand extends Command
      */
     protected function exportJS()
     {
-        copy(
-            __DIR__.'/js/app.js',
-            'app.jsx'
-        );
         foreach ($this->componentJSFiles as $key => $value) {
             if (file_exists($componentJS = $this->getComponentsJsPath($value)) && ! $this->option('force')) {
                 if (! $this->components->confirm("The [$value] js file already exists. Do you want to replace it?")) {
@@ -94,6 +94,19 @@ class ButtonCommand extends Command
             copy(
                 __DIR__.'/js/Store/'.$key,
                 $storeJS
+            );
+        }
+
+        foreach ($this->rootJSFiles as $key => $value) {
+            if (file_exists($rootJS = $this->getStoreJsPath($value)) && ! $this->option('force')) {
+                if (! $this->components->confirm("The [$value] js file already exists. Do you want to replace it?")) {
+                    continue;
+                }
+            }
+
+            copy(
+                __DIR__.'/js/'.$key,
+                $rootJS
             );
         }
     }
@@ -132,6 +145,23 @@ class ButtonCommand extends Command
         
         return implode(DIRECTORY_SEPARATOR, [
             resource_path('js/Store'), $path,
+        ]);
+    }
+
+    /**
+     * Get full Root JS path relative to the application's configured JS path.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    protected function getRootJsPath($path)
+    {
+        if (!is_dir($directory = resource_path('js'))) {
+            mkdir($directory, 0755, true);
+        }
+        
+        return implode(DIRECTORY_SEPARATOR, [
+            resource_path('js'), $path,
         ]);
     }
 }
